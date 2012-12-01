372a116b6ae2e07a8ddc2d227626469b31a3730d
James <e@mailjames.com>
2012-12-01 16:17:44 +0000

    Use random port

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-simpleWebServer
-===============
+#Simple Web Server
 
-A simple webserver for developers
+A simple webserver built using express ideal for development.
+-------------------------------------------------------------
+
+Git clone into each project you want a webserver for...
+
+    cd simpleWebServer
+    node server.js
+
+Uses a random port beween 8000 and 99999 - so you can run multiple sites from their own respective code folder
+
---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var http = require('http');
+var toolbox = require('Toolbox');
+
+var app = express();
+var server = http.createServer(app);
+
+var logger = function(req, res, next) {
+    console.log(req.method + ': ' + req.url);
+    next();
+}
+
+
+app.configure(function(){
+    app.use(logger);
+    app.use(express.static('../'));
+});
+
+var port = toolbox.getRandomNum(8000, 9999);
+
+server.listen(port);
+
+console.log('simpleWebServer listening at http://localhost:%s', port);
