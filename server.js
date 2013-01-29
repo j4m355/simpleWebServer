ec16b74d44f517c63cbf6ca45f413fcbe25d0073
James <e@mailjames.com>
2013-01-29 19:45:32 +0000

    to coffee-script

--- server.js
+++ server.js
@@ -1,22 +1,2 @@
-var express = require('express');
-var http = require('http');
-var toolbox = require('toolbox');
-
-var app = express();
-var server = http.createServer(app);
-
-var logger = function(req, res, next) {
-    console.log(req.method + ': ' + req.url);
-    next();
-}
-
-app.configure(function(){
-    app.use(logger);
-    app.use(express.static('../'));
-});
-
-var port = toolbox.getRandomNum(8000, 9999);
-
-server.listen(port);
-
-console.log('simpleWebServer listening at http://localhost:%s', port);
+require('coffee-script')
+require('./app/app')
